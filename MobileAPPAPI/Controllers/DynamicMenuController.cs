93e34bc5b6c59b02c59f506188d96e23afd8918c
Shyalajajayakumar <113327784+Shyalajajayakumar@users.noreply.github.com>
2022-09-21 18:21:41 +0530

    new controller added
    new controller added

---
+++ MobileAPPAPI/Controllers/DynamicMenuController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MobileAppAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DynamicMenuController : ControllerBase
+    {
+
+        public static Helper objhelper = new Helper();
+        public static string strconn = objhelper.Connectionstring();
+
+
+        [HttpGet]
+        [Route("boparentmenu/{strUserCode}")]
+        public string boparentmenu(string strUserCode)
+        {
+            string json = string.Empty;
+
+            using (SqlConnection dbConn = new SqlConnection(strconn))
+            {
+                string Logdata1 = string.Empty;
+                dbConn.Open();
+               // SqlCommand sqlcommand = new SqlCommand("BO_PARENT_MENU", sqlconnection);
+                string sql = "BO_PARENT_MENU";
+                SqlCommand cmd = new SqlCommand(sql, dbConn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@USERCODE", strUserCode);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+
+                var reader = cmd.ExecuteReader();
+                System.Data.DataTable results = new System.Data.DataTable();
+                results.Load(reader);
+                //string outputval = cmd.Parameters["@outputparam"].Value.ToString();
+                for (int i = 0; i < results.Rows.Count; i++)
+                {
+                    DataRow row = results.Rows[i];
+                    Logdata1 = DataTableToJSONWithStringBuilder(results);
+                }
+                return Logdata1;
+            }
+        }
+
+
+        [HttpGet]
+        [Route("getparentandchildmenu/{strUserCode}/{parentmenu}")]
+        public string getparentandchildmenu(string strUserCode,string parentmenu)
+        {
+            string json = string.Empty;
+
+            using (SqlConnection dbConn = new SqlConnection(strconn))
+            {
+                string Logdata1 = string.Empty;
+                dbConn.Open();
+                // SqlCommand sqlcommand = new SqlCommand("BO_PARENT_MENU", sqlconnection);
+                string sql = "MAPP_BO_DYNAMIC_MENU";
+                SqlCommand cmd = new SqlCommand(sql, dbConn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@USERCODE", strUserCode);
+                cmd.Parameters.AddWithValue("@PARENTMENU", parentmenu);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+
+                var reader = cmd.ExecuteReader();
+                System.Data.DataTable results = new System.Data.DataTable();
+                results.Load(reader);
+                //string outputval = cmd.Parameters["@outputparam"].Value.ToString();
+                for (int i = 0; i < results.Rows.Count; i++)
+                {
+                    DataRow row = results.Rows[i];
+                    Logdata1 = DataTableToJSONWithStringBuilder(results);
+                }
+                return Logdata1;
+            }
+        }
+
+
+
+
+       
+        public string DataTableToJSONWithStringBuilder(DataTable table)
+        {
+            var JSONString = new StringBuilder();
+            if (table.Rows.Count > 0)
+            {
+                JSONString.Append("[");
+                for (int i = 0; i < table.Rows.Count; i++)
+                {
+                    JSONString.Append("{");
+                    for (int j = 0; j < table.Columns.Count; j++)
+                    {
+                        if (j < table.Columns.Count - 1)
+                        {
+                            JSONString.Append("\"" + table.Columns[j].ColumnName.ToString() + "\":" + "\"" + table.Rows[i][j].ToString() + "\",");
+                        }
+                        else if (j == table.Columns.Count - 1)
+                        {
+                            JSONString.Append("\"" + table.Columns[j].ColumnName.ToString() + "\":" + "\"" + table.Rows[i][j].ToString() + "\"");
+                        }
+                    }
+                    if (i == table.Rows.Count - 1)
+                    {
+                        JSONString.Append("}");
+                    }
+                    else
+                    {
+                        JSONString.Append("},");
+                    }
+                }
+                JSONString.Append("]");
+            }
+            return JSONString.ToString();
+        }
+
+
+
+    }
+}
